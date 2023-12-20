66071059923e036dedba89c0078d664136941a7c
marco <49691247+marcoSchr@users.noreply.github.com>
2023-12-20 09:48:18 +0100

    Fix naming for log file

--- linevul/LineVul_reevaluate.sh
+++ linevul/LineVul_reevaluate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-#SBATCH --job-name=linevul_train
-#SBATCH --output=/ukp-storage-1/schroeder_e/linevul_main.txt
+#SBATCH --job-name=linevul_reevaluate
+#SBATCH --output=/ukp-storage-1/schroeder_e/linevul_reevaluate.txt
 #SBATCH --mail-user=marco.schroeder@stud.tu-darmstadt.de
 #SBATCH --mail-type=ALL
 #SBATCH --account=tk-student
