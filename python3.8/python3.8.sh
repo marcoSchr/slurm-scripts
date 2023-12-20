85cb7d13fe89cd33d766c69674cdcb79e7cc3281
marco <49691247+marcoSchr@users.noreply.github.com>
2023-12-20 09:48:18 +0100

    Add pyconfig.h to python include files

--- python3.8/python3.8.sh
+++ python3.8/python3.8.sh
@@ -19,3 +19,4 @@ tar -xzvf Python-3.8.18.tgz
 cd Python-3.8.18
 LD_RUN_PATH="/ukp-storage-1/schroeder_e/sqlite/usr/lib" ./configure LDFLAGS="-L/ukp-storage-1/schroeder_e/sqlite/usr/lib" CPPFLAGS="-I/ukp-storage-1/schroeder_e/sqlite/usr/include"
 LD_RUN_PATH="/ukp-storage-1/schroeder_e/sqlite/usr/lib" make -j 40
+ln -s ../pyconfig.h Include/
