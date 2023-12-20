d7232d42a844504eac49979e13a044c685717974
marco <49691247+marcoSchr@users.noreply.github.com>
2023-12-20 09:48:19 +0100

    Add scripts for reevaluating with vpp

---
+++ linevul/LineVul_reevaluate_vpp_orig_model.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+#SBATCH --job-name=linevul_rq1
+#SBATCH --output=/ukp-storage-1/schroeder_e/linevul_rq1.txt
+#SBATCH --mail-user=marco.schroeder@stud.tu-darmstadt.de
+#SBATCH --mail-type=ALL
+#SBATCH --account=tk-student
+#SBATCH --partition=gpu-tk
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=16GB
+#SBATCH --gres=gpu:1
+
+source /ukp-storage-1/schroeder_e/LineVul/venv/bin/activate
+module load cuda/10.0
+cd /ukp-storage-1/schroeder_e/LineVul/linevul/
+python linevul_main.py \
+  --model_name=12heads_linevul_model.bin \
+  --output_dir=./saved_models \
+  --model_type=roberta \
+  --tokenizer_name=microsoft/codebert-base \
+  --model_name_or_path=microsoft/codebert-base \
+  --do_test \
+  --train_data_file=../data/vpp_dataset/train.csv \
+  --eval_data_file=../data/vpp_dataset/val.csv \
+  --test_data_file=../data/vpp_dataset/test.csv \
+  --block_size 512 \
+  --eval_batch_size 256
+
