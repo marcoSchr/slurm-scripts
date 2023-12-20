2244f3d62b011bb94cc92130504f07092fe95091
marco <49691247+marcoSchr@users.noreply.github.com>
2023-12-20 09:48:17 +0100

    Add linevd_init

---
+++ linevd/linevd_init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#SBATCH --job-name=linevd_init
+#SBATCH --output=/ukp-storage-1/schroeder_e/linevd_init.txt
+#SBATCH --mail-user=marco.schroeder@stud.tu-darmstadt.de
+#SBATCH --mail-type=ALL
+#SBATCH --account=tk-student
+#SBATCH --partition=gpu-tk
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16GB
+#SBATCH --gres=gpu:1
+
+export HOME=/ukp-storage-1/schroeder_e/
+source /ukp-storage-1/schroeder_e/linevd/venv/bin/activate
+module load cuda/10.0
+cd /ukp-storage-1/schroeder_e/linevd/linevd
+./get_data.sh
