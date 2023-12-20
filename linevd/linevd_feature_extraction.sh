a9f8b5661449dcf064272d9d7d5e9f9fac50eff4
marco <49691247+marcoSchr@users.noreply.github.com>
2023-12-20 09:48:17 +0100

    linevd: try to fix prepare function missing files

--- linevd/linevd_feature_extraction.sh
+++ linevd/linevd_feature_extraction.sh
@@ -15,6 +15,8 @@ export HOME=/ukp-storage-1/schroeder_e/
 source /ukp-storage-1/schroeder_e/linevd/venv/bin/activate
 module load cuda/10.0
 cd /ukp-storage-1/schroeder_e/linevd/linevd
+export PYTHONPATH=/ukp-storage-1/schroeder_e/linevd/linevd:$PYTHONPATH
 
+python -c 'import sastvd.helpers.datasets as svdd;svdd.bigvul(False)'
 python sastvd/scripts/prepare.py
 python sastvd/scripts/getgraphs.py
