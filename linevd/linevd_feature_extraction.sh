35b03f355f31b31568c7605e9b0aac93cd860400
marco <49691247+marcoSchr@users.noreply.github.com>
2023-12-20 09:48:17 +0100

    linevd: add feature extraction

---
+++ linevd/linevd_feature_extraction.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --job-name=linevd_feature_extraction
+#SBATCH --output=/ukp-storage-1/schroeder_e/linevd_feature_extraction.txt
+#SBATCH --mail-user=marco.schroeder@stud.tu-darmstadt.de
+#SBATCH --mail-type=ALL
+#SBATCH --account=tk-student
+#SBATCH --partition=gpu-tk
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=16GB
+#SBATCH --gres=gpu:1
+
+export HOME=/ukp-storage-1/schroeder_e/
+source /ukp-storage-1/schroeder_e/linevd/venv/bin/activate
+module load cuda/10.0
+cd /ukp-storage-1/schroeder_e/linevd/linevd
+
+python sastvd/scripts/prepare.py
+python sastvd/scripts/getgraphs.py
